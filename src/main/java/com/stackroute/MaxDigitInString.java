79255666ebc9c4c20beccac46b9d7e7769135637
ubuntu <ubuntu@ip-172-31-83-26.ec2.internal>
2019-11-06 18:19:13 +0000

    MaxDigit_logic

--- src/main/java/com/stackroute/MaxDigitInString.java
+++ src/main/java/com/stackroute/MaxDigitInString.java
@@ -1,15 +1,49 @@
 package com.stackroute;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class MaxDigitInString {
 
 	public static int getMaxDigit(String alphanumeric) {
-		return -2;
+		Integer[] maxDigit = null;
+		Integer maxNum;
+		if(null== alphanumeric || alphanumeric.isEmpty()) {
+			
+			
+			return -1;
+		}
+		
+		alphanumeric = alphanumeric.trim().replaceAll("\\D","");
+		alphanumeric = alphanumeric.trim().replaceAll(" ", "");
+		
+		
+		
+		if(alphanumeric.length()!=0) {
+			
+			maxDigit = new Integer[alphanumeric.length()];
+			for(int i=0;i<alphanumeric.length();i++){
+			maxDigit[i] = alphanumeric.charAt(i) - '0';
+			}
+			 maxNum = Arrays.stream(maxDigit).max(Integer :: compare).get();
+			return maxNum;
+		}
+		return -1;
 	}
 
    	//Do not print anything other than what's expected/asked in problem
     public static void main(String[] args) {
     	//Use Scanner to get input from console
- 
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+       int i = getMaxDigit(input);
+       if(i== -1) {
+    	   System.out.println("No digits in string");
+       }
+       else {
+    	   System.out.println("Maxdigit:" + i);
+       }
     
     }
 }
